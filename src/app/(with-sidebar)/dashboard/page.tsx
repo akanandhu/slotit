c58ef8e0693ff78b4c0bbb76d53445e1e017a397
akanandhu <akananthu11@gmail.com>
2025-02-16 14:47:55 +0530

    feat: date picker

--- src/app/(with-sidebar)/dashboard/page.tsx
+++ src/app/(with-sidebar)/dashboard/page.tsx
@@ -1,19 +1,17 @@
 import { Button } from "@/components/ui/button";
-import { CalendarDays, CalendarPlus2 } from "lucide-react";
+import { DateRangePicker } from "@/components/ui/date-range-picker";
+import { CalendarPlus2 } from "lucide-react";
 
 const HomePage = () => {
   return (
     <div>
-      <div className="flex justify-between">
+      <div className="flex flex-col gap-4 md:gap-0 md:flex-row justify-between">
         <div>
           <h3 className="font-medium text-2xl text-[#0F1016]">Welcome Jay</h3>
-          <h6 className="text-[#074762] text-lg">Manage your saloon easily</h6>
+          <h6 className="text-[#074762]">Manage your saloon easily</h6>
         </div>
-        <div className="flex gap-3 items-end">
-          <Button variant={"secondary"} rounded={"md"}>
-            <CalendarDays />
-            Select Dates
-          </Button>
+        <div className="flex flex-col md:flex-row gap-3 md:items-end">
+          <DateRangePicker />
           <Button rounded={"md"}>
             <CalendarPlus2 />
             Add Appointment
